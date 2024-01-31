8339ae30f744af0ee3266628d6d738a3b0b0fd6b
Flossy <sfloess@redhat.com>
2024-01-31 14:51:34 -0500

    WIP - Latest

--- src/main/java/org/solenopsis/soap/service/ServiceEnum.java
+++ src/main/java/org/solenopsis/soap/service/ServiceEnum.java
@@ -1,30 +1,27 @@
 package org.solenopsis.soap.service;
 
-import org.solenopsis.soap.SubUrlEnum;
 import org.solenopsis.soap.service.factory.ServiceFactoryEnum;
 
 /**
  * Joins together import pieces of service info.
- * 
+ *
  * @author sfloess
  */
 public enum ServiceEnum {
-    APEX(ServiceQNameEnum.APEX, ServiceWsdlEnum.APEX, SubUrlEnum.APEX, ServiceFactoryEnum.APEX),
-    ENTERPRISE(ServiceQNameEnum.ENTERPRISE, ServiceWsdlEnum.ENTERPRISE, SubUrlEnum.ENTERPRISE, ServiceFactoryEnum.ENTERPRISE),
-    METADATA(ServiceQNameEnum.METADATA, ServiceWsdlEnum.METADATA, SubUrlEnum.METADATA, ServiceFactoryEnum.METADATA),
-    PARTNER(ServiceQNameEnum.PARTNER, ServiceWsdlEnum.PARTNER, SubUrlEnum.PARTNER, ServiceFactoryEnum.PARTNER),
-    TOOLING(ServiceQNameEnum.TOOLING, ServiceWsdlEnum.TOOLING, SubUrlEnum.TOOLING, ServiceFactoryEnum.TOOLING),
+    APEX(ServiceQNameEnum.APEX, ServiceWsdlEnum.APEX, ServiceFactoryEnum.APEX),
+    ENTERPRISE(ServiceQNameEnum.ENTERPRISE, ServiceWsdlEnum.ENTERPRISE, ServiceFactoryEnum.ENTERPRISE),
+    METADATA(ServiceQNameEnum.METADATA, ServiceWsdlEnum.METADATA,  ServiceFactoryEnum.METADATA),
+    PARTNER(ServiceQNameEnum.PARTNER, ServiceWsdlEnum.PARTNER, ServiceFactoryEnum.PARTNER),
+    TOOLING(ServiceQNameEnum.TOOLING, ServiceWsdlEnum.TOOLING, ServiceFactoryEnum.TOOLING),
     ;
 
     private final ServiceQNameEnum qname;
     private final ServiceWsdlEnum wsdl;
-    private final SubUrlEnum subUrl;
     private final ServiceFactoryEnum factory;
 
-    private ServiceEnum(final ServiceQNameEnum qname, final ServiceWsdlEnum wsdl, final SubUrlEnum subUrl, ServiceFactoryEnum factory) {
+    private ServiceEnum(final ServiceQNameEnum qname, final ServiceWsdlEnum wsdl, ServiceFactoryEnum factory) {
         this.qname = qname;
         this.wsdl = wsdl;
-        this.subUrl = subUrl;
         this.factory = factory;
     }
 
@@ -34,10 +31,6 @@ public enum ServiceEnum {
 
     public ServiceWsdlEnum getWsdl() {
         return wsdl;
-    }
-
-    public SubUrlEnum getSubUrl() {
-        return subUrl;
     }
 
     public ServiceFactoryEnum getFactory() {
